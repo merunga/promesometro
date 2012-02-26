1677e4f3ea746a18b5f4eba5f5fc06a641177dcb
Mariano Crowe <merunga@gmal.com>
2012-02-26 05:05:35 -0500

    return tu prev page when login in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   add_crumb "Home", '/'
+
+  def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+  end
 end
