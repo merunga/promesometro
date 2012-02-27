bd80b0dbebefa803ae05b3cf30bbc08a089f36e9
Mariano Crowe <merunga@gmal.com>
2012-02-27 10:50:37 -0500

    ominiauth no origin home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource_or_scope)
     if request.env['omniauth.origin']
       request.env['omniauth.origin']
+    else
+      root_url
     end
   end
 end
