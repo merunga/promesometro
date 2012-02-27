c4586574286a7b8e7cb8546c67562d7b1079f578
Mariano Crowe <merunga@gmal.com>
2012-02-27 09:54:47 -0500

    condicional fb metadata

--- app/helpers/social/meta_data_helper.rb
+++ app/helpers/social/meta_data_helper.rb
@@ -2,11 +2,10 @@ module Social::MetaDataHelper
   def node_meta_data(model,id)
      case model
        when 'declaration' then
-         @d = Declaration.find(params[:id])
+         @d = Declaration.find(id)
          @p = @d.promise
          render :partial => 'social/meta_data_tags', :locals => {
-             :model => 'declaration',
-             :link => promesa_url(@p.slug),
+             :link => promesa_url(@p.slug)+"/?md[]=#{model}&md[]=#{id}",
              :title => "Declaracion de #{@p.official.name} acerca de #{@p.title}",
              :desc => @d.body
          }
@@ -14,6 +13,17 @@ module Social::MetaDataHelper
   end
 
   def page_meta_data
-    render 'social/meta_data_tags', :title => '', :desc => '', :link => ''
+    render 'social/meta_data_tags',
+         :title => 'promesometro.pe',
+         :desc => 'Mide y discute el progreso de las promesas de tus representantes',
+         :link => 'http://promesometro.pe'
+  end
+
+  def meta_data_tags(node=nil)
+    if node
+      node_meta_data node[0], node[1]
+    else
+      page_meta_data
+    end
   end
 end
