e114b718e6a9c6b663f79a482cba98cdde56a208
Mariano Crowe <merunga@gmal.com>
2012-02-27 09:22:27 -0500

    conditional notification send

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -8,7 +8,27 @@ class CommentMailer < ActionMailer::Base
     end
     @comment = comment
     @display_name = display_name
-    mail(:from => 'merunga@gmail.com', :bcc => emails_for_comment(comment), :subject => I18n.t('muck.comments.new_comment_email_subject', :name => display_name, :application_name => 'promesometro')) do |format|
+    emails = emails_for_comment(comment)
+
+    mail(:from => 'merunga@gmail.com', :bcc => emails_for_comment(comment),
+         :subject => I18n.t('muck.comments.new_comment_email_subject',
+         :name => display_name, :application_name => 'promesometro')) do |format|
+      format.html
+      format.text
+    end
+  end
+
+  def new_from_emails(emails, curr_user)
+    if curr_user and curr_user.screen_name
+      display_name = curr_user.screen_name
+    else
+      display_name = I18n.t('muck.comment.anonymous')
+    end
+    @display_name = display_name
+
+    mail(:from => 'merunga@gmail.com', :bcc => emails,
+         :subject => I18n.t('muck.comments.new_comment_email_subject',
+                            :name => display_name, :application_name => 'promesometro')) do |format|
       format.html
       format.text
     end
@@ -20,7 +40,7 @@ class CommentMailer < ActionMailer::Base
       #Comment.find_comments_for_parent(comment) do |c|
       parent = comment.commentable_type.classify.constantize.find(comment.commentable_id)
       parent.comment_threads.each do |c|
-        emails << c.user.email unless emails.include?(c.user.email) if c.user
+        emails << c.user.email unless emails.include?(c.user.email) if c.user and c.user.send_notifications
       end
       emails
     end
