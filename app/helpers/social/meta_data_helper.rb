85927ff9b4e76d8db96e5e3c93cbac9cfd9dd1ef
Mariano Crowe <merunga@gmal.com>
2012-02-27 10:21:54 -0500

    condicional fb metadata

--- app/helpers/social/meta_data_helper.rb
+++ app/helpers/social/meta_data_helper.rb
@@ -1,19 +1,19 @@
 module Social::MetaDataHelper
   def node_meta_data(model,id)
      case model
-       when 'declaration' then
+       when 'd' then
          @d = Declaration.find(id)
          @p = @d.promise
          render :partial => 'social/meta_data_tags', :locals => {
-             :link => promesa_url(@p.slug)+"/?mdm=#{model}&mdi=#{id}",
+             :link => promesa_url(@p.slug)+"/?mdm=d&mdi=#{id}",
              :title => "Declaracion de #{@p.official.name} acerca de #{@p.title}",
              :desc => @d.body
          }
 
-       when 'promise' then
+       when 'p' then
          @p = Promise.find(id)
          render :partial => 'social/meta_data_tags', :locals => {
-             :link => promesa_url(@p.slug)+"/?mdm=#{model}&mdi=#{id}",
+             :link => promesa_url(@p.slug)+"/?mdm=p&mdi=#{id}",
              :title => @p.title,
              :desc => @p.description
          }
@@ -22,9 +22,9 @@ module Social::MetaDataHelper
 
   def page_meta_data
     render 'social/meta_data_tags',
-         :title => 'promesometro.pe',
-         :desc => 'Mide y discute el progreso de las promesas de tus representantes',
-         :link => 'http://promesometro.pe'
+       :title => 'promesometro.pe',
+       :desc => 'Mide y discute el progreso de las promesas de tus representantes',
+       :link => 'http://promesometro.pe'
   end
 
   def meta_data_tags(model,id)
@@ -33,5 +33,22 @@ module Social::MetaDataHelper
     else
       page_meta_data
     end
+  end
+
+  def promise_social_widgets(p)
+    render 'social/node_widgets',
+      :link => promesa_url(p.slug)+"/?mdm=p&mdi=#{p.id}",
+      :title => p.title,
+      :desc => p.description,
+      :width => 280
+  end
+
+  def declaration_social_widgets(d)
+    p = d.promise
+    render 'social/node_widgets',
+      :link => promesa_url(p.slug)+"/?mdm=d&mdi=#{d.id}",
+      :title => "Declaracion de #{p.official.name} acerca de #{p.title}",
+      :desc => d.body,
+      :width => 280
   end
 end
