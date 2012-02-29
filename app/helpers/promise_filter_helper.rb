315f994725dd3907d7c80749c3ac9f4ba9905911
Mariano Crowe <merunga@gmal.com>
2012-02-29 02:38:56 -0500

    flash messages

--- app/helpers/promise_filter_helper.rb
+++ app/helpers/promise_filter_helper.rb
@@ -1,5 +1,6 @@
 module PromiseFilterHelper
   def input_value(params,param_name)
-    (params[:search] == 'topic')? '': (params[:search]? params[:search][param_name]: nil)
+    (params[:search] == 'topic')? '':
+        (params[:search]? params[:search][param_name]: nil)
   end
 end
