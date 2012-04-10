b0f216e7b3af476acdcd629e988214568f9b3e2a
Mariano Crowe <merunga@gmal.com>
2012-04-09 21:37:39 -0500

    compile assets on produ

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Promesometro::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -79,11 +79,6 @@ Promesometro::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  
-  config.assets.compile = true
-  
-  # Do not compress assets
-  config.assets.compress = false
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
